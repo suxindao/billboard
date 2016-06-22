f13e393ab72a640b127cc07a3d6beea89ee30417
BDad_Lei <125651497@qq.com>
2016-06-22 16:05:11 +0800

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -140,7 +140,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
 
       .state('login', {
         url: '/login',
-        templateUrl: 'templates/matchn.html',
+        templateUrl: 'templates/managen.html',
         controller: 'LoginCtrl'
       })
 
@@ -159,7 +159,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
 
       .state('main', {
         url: '/main',
-        templateUrl: 'templates/matchn.html',
+        templateUrl: 'templates/managen.html',
         controller: 'MainCtrl'
       })
 
