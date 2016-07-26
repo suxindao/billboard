2b3f9fc3a0a9672323488e62e97f92dd6d86c36c
leiruiqi <125651497@qq.com>
2016-07-26 13:30:46 +0800

    no message

---
+++ plugin_install.sh
@@ -0,0 +1,1 @@
+ionic plugin add cordova-plugin-device cordova-plugin-console cordova-plugin-whitelist cordova-plugin-splashscreen cordova-plugin-statusbar ionic-plugin-keyboard cordova-plugin-file-transfer cordova-plugin-file MD5forFile cordova-plugin-inappbrowser cordova-hot-code-push-plugin cordova-hot-code-push-local-dev-addon cordova-imagePicker
