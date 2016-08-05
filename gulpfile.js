836ddffc00b211999d7b31163830e58d20beef98
suxindao <suxindao@126.com>
2016-08-05 16:34:47 +0800

    add source maps for sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
 var sdir = "./bower_components/";
-var ddir = "./www/lib/";
+var jsdir = "./www/lib/";
+var cssdir = "./www/css/";
 
 var filesToMove = [
   sdir + '/ionic/js/ionic.bundle.js',
@@ -21,27 +23,36 @@ var filesToMove = [
   sdir + '/Swiper/dist/js/swiper.js'
 ];
 
-gulp.task('default', ['cpjs', 'sass']);
+gulp.task('default', ['clean', 'cpjs', 'sass']);
 
-gulp.task('clean', function () {
-  return gulp.src([ddir], {read: false})
+gulp.task('clean', ['cleanJS', 'cleanCSS']);
+
+gulp.task('cleanJS', function () {
+  return gulp.src([jsdir], {read: false})
     .pipe(clean());
 });
 
-gulp.task('cpjs', ['clean'], function () {
-  return gulp.src(filesToMove)
-    .pipe(gulp.dest(ddir));
+gulp.task('cleanCSS', function () {
+  return gulp.src([cssdir], {read: false})
+    .pipe(clean());
 });
 
-gulp.task('sass', function (done) {
+gulp.task('cpjs', ['cleanJS'], function () {
+  return gulp.src(filesToMove)
+    .pipe(gulp.dest(jsdir));
+});
+
+gulp.task('sass', ['cleanCSS'], function (done) {
   gulp.src('./scss/ionic.app.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    // .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({extname: '.min.css'}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
