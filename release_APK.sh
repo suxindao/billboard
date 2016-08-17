b447bbfd35cb6e085edc6260c9d30d4c8b3c9fe1
suxindao <suxindao@126.com>
2016-08-17 14:40:37 +0800

    no message

--- release_APK.sh
+++ release_APK.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 export apk_path=/Users/suxindao/work/billboard/platforms/android/build/outputs/apk
+export build_path=build
 export DATE=$(date +"%Y%m%d")
+
+if [ ! -d $build_path ]; then
+  mkdir $build_path
+fi
+
+rm $build_path/MagicManager-release-$DATE.apk
 
 ionic build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore $apk_path/android-release-unsigned.apk panocean
-rm ./MagicManager-release-$DATE.apk
-zipalign -v 4 $apk_path/android-release-unsigned.apk ./MagicManager-release-$DATE.apk
+zipalign -v 4 $apk_path/android-release-unsigned.apk $build_path/MagicManager-release-$DATE.apk
